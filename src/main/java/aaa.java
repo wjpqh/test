614def83cd71471333ae1d18fc0befb2b6b23a0c
wjp <1225698630@qq.com>
2022-03-03 21:59:48 +0800

    First commit

---
+++ src/main/java/aaa.java
@@ -0,0 +1,15 @@
+public class aaa {
+    public int a;
+
+    public aaa(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
---
+++ src/main/java/oom.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class oom {
+    public static void main(String[] args) {
+            String  s="ababc";
+            int max=0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int k= i+1; k< s.length(); k++) {
+                String now=s.substring(i,k);
+                if(ishuiwen(now)&&now.length()>max){
+                    max=now.length();
+                }
+
+            }
+        }
+
+        System.out.println(max);
+
+    }
+
+    public static boolean ishuiwen(String s){
+       char[] chars= s.toCharArray();
+        for (int i = 0; i <chars.length/2 ; i++) {
+            if(chars[i]!=chars[chars.length-1-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
