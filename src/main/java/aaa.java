a170cf9648942ba80c208c71779700e7b0b08213
wjp <1225698630@qq.com>
2022-03-08 22:17:00 +0800

    test

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -1,15 +1,15 @@
 public class aaa {
     public int a;
 
-    public aaa(int a) {
-        this.a = a;
+    public aaa(int b) {
+        this.a = b;
     }
 
     public int getA() {
         return a;
     }
 
-    public void setA(int a) {
-        this.a = a;
+    public void setA(int c) {
+        this.a = c;
     }
 }
