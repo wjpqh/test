0fa9dcdab621a0ea65f68feeb3b1284c733dbc7d
wjp <1225698630@qq.com>
2022-03-08 22:09:20 +0800

    test

---
+++ src/main/java/bb.java
@@ -0,0 +1,15 @@
+public class bb {
+    public int a;
+
+    public bb(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
